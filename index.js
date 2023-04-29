d96de0b50ec81be44cd6d49c1562f12bea29ad5f
rsnirob <niroborin111@gmail.com>
2023-04-29 08:10:34 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -13,7 +14,37 @@ app.get('/', (req, res) => {
 })
 
 app.get('/categories', (req, res) => {
+    // console.log(categories);
     res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+    // console.log(news);
+    res.send(news);
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    // console.log(selectedNews);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    // const categoryNews = news.filter(n => n.category_id === id);
+    // // console.log(categoryNews);
+    // res.send(categoryNews);
+
+    if (id === 0) {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        // console.log(categoryNews);
+        res.send(categoryNews);
+    }
 })
 
 app.listen(port, () => {
